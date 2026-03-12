080f01bb3ed9fabcf90fd2088459ffd92a22fcda
Barak360 <barakjohnson360@gmail.com>
2026-03-12 11:46:24 +0100

    Refactor Sulaiman.java to remove class and main method
    Removed the class definition and main method, retaining print statements.

--- Scripts/Sulaiman.java
+++ Scripts/Sulaiman.java
@@ -1,6 +1,4 @@
-public class sulaiman_musa{public static void main(string[] args) {
-     system.out.println("sulaiman musa");
-     system.out.println("sulaimankroos@gmail.com");
-     system.out.println("@sulaimankroos");
-     system.out.println("epigenomics");
-}
+system.out.println("sulaiman musa");
+system.out.println("sulaimankroos@gmail.com");
+system.out.println("@sulaimankroos");
+system.out.println("epigenomics");
